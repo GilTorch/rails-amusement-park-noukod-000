246e069837bc7aa198366c0589bd871b946cf22e
Gilbert Torchon <gilbert@noukod.com>
2019-02-01 19:39:30 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -6,7 +6,7 @@ class RidesController < ApplicationController
 
     @ride=Ride.find(params[:id])
 
-    @ride.take_ride
+    flash[:messgae]=@ride.take_ride
     redirect_to user_path(@ride.user)
   end
 
