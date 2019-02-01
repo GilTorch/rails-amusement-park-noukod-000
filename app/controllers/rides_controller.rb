728952573976331ff3d6082c1b8c3aec8ace6c3b
Gilbert Torchon <gilbert@noukod.com>
2019-02-01 19:35:51 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -6,7 +6,7 @@ class RidesController < ApplicationController
 
     @ride=Ride.find(params[:id])
     flash[:message]="Thanks for riding the #{@ride.attraction.name}"
-    puts flash[:message].inspect
+
     @ride.take_ride
     redirect_to user_path(@ride.user)
   end
