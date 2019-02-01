a85afcebaf9787ed02d1287b9a9581b205519a72
Gilbert Torchon <gilbert@noukod.com>
2019-02-01 19:18:48 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
     resources :users,only:[:index,:show,:create,:new,:update]
     resources :attractions,only:[:index,:show,:new,:create,:edit,:update]
     resources :rides,only:[:index,:show,:new,:update,:create]
-=======
-    resources :users,only:[:index,:show,:create,:new]
-    resources :attractions,only:[:index,:show,:new,:create,:edit,:update]
->>>>>>> e103fd32bd60cc1c57687c39a215d2c68b6b3837
+
     get "/signin",to:"sessions#signin"
     post "/signin",to:"sessions#create"
     get "/logout",to:"sessions#destroy"
