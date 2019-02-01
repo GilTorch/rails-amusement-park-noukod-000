f03fbc59f2f785579d0dc9496c7c3a6f32575474
Gilbert Torchon <gilbert@noukod.com>
2019-02-01 19:22:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-<<<<<<< HEAD
 
   helper_method :current_user
 
@@ -11,10 +10,5 @@ class ApplicationController < ActionController::Base
    else
      nil
    end
-=======
-  def current_user
-   return unless session[:user_id]
-   @current_user ||= User.find(session[:user_id])
->>>>>>> e103fd32bd60cc1c57687c39a215d2c68b6b3837
  end
 end
